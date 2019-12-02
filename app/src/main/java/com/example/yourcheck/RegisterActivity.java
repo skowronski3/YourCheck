063f1fb24e0108d1e1bfb9fe369f57defda5cbde
skowronski3 <skowronm@umich.edu>
2019-12-01 20:53:11 -0500

    Initial Commit

--- app/src/main/java/com/example/yourcheck/RegisterActivity.java
+++ app/src/main/java/com/example/yourcheck/RegisterActivity.java
@@ -8,7 +8,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); 
         setContentView(R.layout.activity_register);
     }
 }
