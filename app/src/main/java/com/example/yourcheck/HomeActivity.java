ede4677b2272a67d95a05f7eb0f46a59479b879d
skowronski3 <skowronm@umich.edu>
2019-12-06 03:06:05 -0500

    Login, Register, and partial Home activity.

--- app/src/main/java/com/example/yourcheck/HomeActivity.java
+++ app/src/main/java/com/example/yourcheck/HomeActivity.java
@@ -1,14 +1,70 @@
 package com.example.yourcheck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView textViewUsernameHome;
+    Button buttonRequestCheckHome, buttonCheckHistoryHome, buttonRunCheckHome, buttonViewCheckHome, buttonSendVerificationHome;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        textViewUsernameHome = findViewById(R.id.textViewUsernameHome);
+        buttonRequestCheckHome = findViewById(R.id.buttonRequestCheckHome);
+        buttonCheckHistoryHome = findViewById(R.id.buttonCheckHistoryHome);
+        buttonRunCheckHome = findViewById(R.id.buttonRunCheckHome);
+        buttonViewCheckHome = findViewById(R.id.buttonViewCheckHome);
+        buttonSendVerificationHome = findViewById(R.id.buttonSendVerificationHome);
+
+        textViewUsernameHome.setText("Hello, " + currentUser.getEmail());
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.itemHome) {
+            Toast.makeText(this, "Already in Home!", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.itemLogout) {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(this, "Logged out!", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/yourcheck/RegisterActivity.java
+++ app/src/main/java/com/example/yourcheck/RegisterActivity.java
@@ -1,14 +1,80 @@
 package com.example.yourcheck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText editTextEmailRegister, editTextPasswordRegister, editTextVerifyPasswordRegister;
+    Button buttonCreateAccountRegister, buttonLoginRegister;
+    String email, password, verifyPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); 
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        editTextEmailRegister = findViewById(R.id.editTextEmailRegister);
+        editTextPasswordRegister = findViewById(R.id.editTextPasswordRegister);
+        editTextVerifyPasswordRegister = findViewById(R.id.editTextVerifyPasswordRegister);
+
+        buttonCreateAccountRegister = findViewById(R.id.buttonCreateAccountRegister);
+        buttonLoginRegister = findViewById(R.id.buttonLoginRegister);
+
+        buttonCreateAccountRegister.setOnClickListener(this);
+        buttonLoginRegister.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        email = editTextEmailRegister.getText().toString();
+        password = editTextPasswordRegister.getText().toString();
+        verifyPassword = editTextVerifyPasswordRegister.getText().toString();
+
+        final Intent loginIntent = new Intent(this,LoginActivity.class);
+
+        switch (view.getId()){
+
+            case R.id.buttonCreateAccountRegister:
+                    if (password.equals(verifyPassword)){
+                        mAuth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            //Registration success, notify user
+                                            Toast.makeText(RegisterActivity.this, "Registration successful! You can now log in with your email and password.", Toast.LENGTH_LONG).show();
+                                            startActivity(loginIntent);
+                                        } else {
+                                            //Registration success, notify user
+                                            Toast.makeText(RegisterActivity.this, "Registration failure, please try again.", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                    } else
+                        Toast.makeText(this, "Passwords don't match!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.buttonLoginRegister:
+                startActivity(loginIntent);
+                break;
+        }
     }
 }
